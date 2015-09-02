794d390462719851c802ed023ddfbe96a3e307f5
Steven <steventhon94@gmail.com>
2015-09-02 09:12:36 -0700

    Hardcode dummy songs

--- main.js
+++ main.js
@@ -10,3 +10,9 @@ function song(title, artist, album) {
 song.prototype.add = function() {
   playlist.push(this);
 };
+
+var song1 = new song("Title1", "Artist", "Album");
+var song2 = new song("Title2", "Artist", "Album");
+var song3 = new song("Title3", "Artist", "Album");
+var song4 = new song("Title4", "Artist", "Album");
+var song5 = new song("Title5", "Artist", "Album");
