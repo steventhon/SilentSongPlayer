eeec4cb5734dce7de2f6b50f5505fc61fb1ec446
Steven <steventhon94@gmail.com>
2015-09-02 09:30:46 -0700

    Add comments, capitalize "song", and add vars

--- main.js
+++ main.js
@@ -1,29 +1,35 @@
 $(document).ready(function() {
   var playlist = [];
+  var shuffle, prev, next;
   
-  function song(title, artist, album) {
+  // Song constructor
+  function Song(title, artist, album) {
     this.title = title;
     this.artist = artist;
     this.album = album;
     played = false;
   }
   
-  song.prototype.add = function() {
+  // Function to append this song to playlist array
+  Song.prototype.add = function() {
     playlist.push(this);
   };
   
-  var song1 = new song("Title1", "Artist", "Album");
-  var song2 = new song("Title2", "Artist", "Album");
-  var song3 = new song("Title3", "Artist", "Album");
-  var song4 = new song("Title4", "Artist", "Album");
-  var song5 = new song("Title5", "Artist", "Album");
+  // Hardcode random Songs
+  var song1 = new Song("Title1", "Artist", "Album");
+  var song2 = new Song("Title2", "Artist", "Album");
+  var song3 = new Song("Title3", "Artist", "Album");
+  var song4 = new Song("Title4", "Artist", "Album");
+  var song5 = new Song("Title5", "Artist", "Album");
   
+  // Append Songs to playlist
   song1.add();
   song2.add();
   song3.add();
   song4.add();
   song5.add();
   
+  // Plays Songs in random order without repeats
   for (var i = 0, len = playlist.length, obj; i < len; i++) {
     do {
       obj = playlist[Math.floor(Math.random() * len)];
